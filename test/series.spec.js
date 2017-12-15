2e6d9da20f80d3af85940af73dae95881b4715a6
yanickrochon <yanick.rochon@gmail.com>
2017-12-15 11:53:27 -0500

    Added missing test + added .npmignore file

--- test/series.spec.js
+++ test/series.spec.js
@@ -16,8 +16,12 @@ describe('Testing series', () => {
 
 
   it('should be reciprocal (array > string > array)', () => {
-
-
+    [
+      [], [0], [1],
+      [1, 2], [-9, -8],
+      [0,1,2,3], [-23, -22, -21, -20, -19],
+      [1, 2, 3, 4, 11, 12, 13, 14, 17], [-7, -6, -5, -4, -3, 1, 2, 3, 4, 5, 6]
+    ].forEach(value => assert.deepStrictEqual( series.parse( series.compile( value )), value) );
   });
 
 });
